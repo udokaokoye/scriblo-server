dfa2887a8aa0dcc26b6a015ec3c7f0170127286b
udokaokoye <leviokoye.com>
2023-05-24 00:57:26 -0400

    added email sender

--- connection.php
+++ connection.php
@@ -1,14 +1,14 @@
 <?php
 // connect to databse boilerplate
-// $servername = "localhost";
-// $username = "u896018919_scribloADMIN";
-// $password = "2P=u>3LQ*S@N";
-// $dbname = "u896018919_scriblo";
-
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "scriblo";
+$username = "u896018919_scribloADMIN";
+$password = "2P=u>3LQ*S@N";
+$dbname = "u896018919_scriblo";
+
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "scriblo";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
