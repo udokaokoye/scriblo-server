8e719faac99a24ae7c92a0c0435f1fee96258825
udokaokoye <leviokoye.com>
2023-05-24 01:05:51 -0400

    update

--- emailclient.php
+++ emailclient.php
@@ -6,7 +6,7 @@ class EmailSender {
         $headers = "From: $fromName <$from>" . "\r\n";
         $headers .= "Reply-To: $from" . "\r\n";
         // $headers .= "MIME-Version: 1.0" . "\r\n";
-        // $headers .= "Content-Type: text/html; charset=UTF-8" . "\r\n";
+        $headers .= "Content-Type: text/html; charset=UTF-8" . "\r\n";
 
         // Send email
         $mailSent = mail($to, $subject, $message, $headers);
