2cc43b8a4f335bdcfb4a66bc9b9594c9fba7a75b
udokaokoye <leviokoye.com>
2023-05-26 10:13:40 -0400

    still figuring things out

---
+++ models/Post.php
@@ -0,0 +1,27 @@
+<?php
+
+class Post {
+    // DB stuff
+    private $conn;
+    private $table = 'posts';
+
+    // Post Properties
+    public $id;
+    public $category_id;
+    public $category_name;
+    public $title;
+    public $body;
+    public $author;
+    public $created_at;
+
+    // Constructor with DB
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    // Get Posts
+    // public function read() {
+    //     // create query
+    //     $query = 'SE'
+    // }
+}
