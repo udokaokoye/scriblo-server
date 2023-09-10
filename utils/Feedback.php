5ae30638ac3d940f35878edf5fcca89062c6c400
udokaokoye <leviokoye.com>
2023-09-10 12:45:46 -0400

    - add api for feedback system - added feature that sends user email after signup

---
+++ utils/Feedback.php
@@ -0,0 +1,34 @@
+<?php
+include '../config/database.php';
+include_once './ResponseHandler.php';
+$method = $_SERVER['REQUEST_METHOD'];
+
+
+if ($method == 'POST') {
+    $database = new Database();
+    $db = $database->connect();
+    if (!isset($_POST['type']) || !isset($_POST['page']) || !isset($_POST['problem']) || !isset($_POST['reproduction']) || !isset($_POST['date'])) {
+        echo ResponseHandler::sendResponse(400, "Feedback Type, Page, Problem, Reproduction and Date is required.");
+        return;
+    }
+    $feedbackType = $_POST['type'];
+    $feedbackPage = $_POST['page'];
+    $feedbackProblem = $_POST['problem'];
+    $feedbackReproduction = $_POST['reproduction'];
+    $feedbackDate = $_POST['date'];
+
+
+
+    $query = "INSERT INTO feedbacks (`type`, `page`, `problem`, `reproduction`, `created_at`) VALUES (?, ?, ?, ? ,?)";
+    $stmt = $db->prepare($query);
+    $stmt->execute([$feedbackType, $feedbackPage, $feedbackProblem, $feedbackReproduction, $feedbackDate]);
+
+    if ($stmt) {
+        echo ResponseHandler::sendResponse(200, "Feedback received");
+    } else {
+        echo ResponseHandler::sendResponse(500, "Request Failed");
+        return;
+    }
+} else {
+    echo ResponseHandler::sendResponse(400, "Invaild Request");
+}
